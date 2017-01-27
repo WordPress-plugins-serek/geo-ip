51e1549f5c4d1d12c6316e21dc04e88b04f840fc
root <root@serek.eu>
2017-01-27 23:56:18 +0000

    Initial commit

---
+++ geo-ip.php
@@ -0,0 +1,23 @@
+<?php
+   /*
+   Plugin Name: Geo IP
+   Plugin URI: https://odd-one-out.serek.eu
+   Description: Gets the country code from visiters IP adress using an AJAX call to freegeoip.net. Returns other custom data. Stores the data in a cookie that expires in 24 hours
+   Version: 1.0
+   Author: Poul Serek
+   Author URI: https://odd-one-out.serek.eu
+   License: GPL2
+   */
+
+function add_assets() {
+	wp_register_script( 'geoip-script',  plugin_dir_url( __FILE__ ) . 'assets/geoip.js' );
+        wp_enqueue_script('geoip-script');
+}
+add_action('wp_enqueue_scripts', 'add_assets');
+
+function wpse_4367231_wp_head(){
+    ?>
+        <link rel="preconnect" href="//freegeoip.net">
+    <?php
+}
+add_action('wp_head', 'wpse_4367231_wp_head', 0);
